72a46164f0c3260716fa1bdc8624bbbd4e282a2f
Lucas Nobrega <lucas.sobrinho@objective.com.br>
2022-01-17 10:35:48 -0300

    Add project files.

---
+++ src/services/NSE.Catalogo.API/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using NSE.Catalogo.API.Models;
+using NSE.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Interfaces
+{
+    public interface IProdutoRepository: IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterTodos();
+        Task<Produto> ObterPorId(Guid id);
+        void Adicionar(Produto produto);
+        void Atualizar(Produto produto);
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Services/Service.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using NSE.WebApp.MVC.Extensions;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public abstract class Service
+    {
+        protected StringContent ObterConteudo(object dado)
+        {
+            return new StringContent(
+                JsonConvert.SerializeObject(dado),
+                Encoding.UTF8,
+                "application/json");
+        }
+
+        protected async Task<T> DeserializarObjetoResponse<T>(HttpResponseMessage responseMessage)
+        {
+            return JsonConvert.DeserializeObject<T>(await responseMessage.Content.ReadAsStringAsync());
+        }
+
+        protected bool TratarErrosResponse(HttpResponseMessage response)
+        {
+            switch ((int)response.StatusCode)
+            {
+                case 401:
+                case 403:
+                case 404:
+                case 500:
+                    throw new CustomHttpRequestException(response.StatusCode);
+                case 400:
+                    return false;
+            }
+
+            response.EnsureSuccessStatusCode();
+            return true;
+        }
+    }
+}
