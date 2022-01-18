b3c55ef79cff6c4b1f544bd101fb1b040357e682
Lucas Nobrega <lucas.sobrinho@objective.com.br>
2022-01-18 16:18:42 -0300

    many changes

--- src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
+++ src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NSE.Catalogo.API.Interfaces;
 using NSE.Catalogo.API.Models;
+using NSE.WebApi.Core.Identidade;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 namespace NSE.Catalogo.API.Controllers
 {
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class CatalogoController : Controller
     {
         private readonly IProdutoRepository _produtoRepository;
@@ -19,14 +20,14 @@ namespace NSE.Catalogo.API.Controllers
             _produtoRepository = produtoRepository;
         }
 
-        //[AllowAnonymous]
+        [AllowAnonymous]
         [HttpGet("catalogo/produtos")]
         public async Task<IEnumerable<Produto>> Index()
         {
             return await _produtoRepository.ObterTodos();
         }
 
-        //[ClaimsAuthorize("Catalogo", "Ler")]
+        [ClaimsAuthorize("Catalogo", "Ler")]
         [HttpGet("catalogo/produtos/{id}")]
         public async Task<Produto> ProdutoDetalhe(Guid id)
         {
